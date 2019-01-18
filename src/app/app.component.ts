d50b60076c0ef05aabd74bad86e0e3e0ffcd484b
Hugo <hugo.lavergne@devinci.fr>
2019-01-18 21:25:20 +0100

    application
    lent (a voir pourquoi)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
+
+const settings = {timestampsInSnapshots: true
+};
+const config = {
+  apiKey: "AIzaSyCngLLRy3BU6Z-egcX4Y8RNhmFLTjWowZ4",
+  authDomain: "projetlourd.firebaseapp.com",
+  databaseURL: "https://projetlourd.firebaseio.com",
+  projectId: "projetlourd",
+  storageBucket: "projetlourd.appspot.com",
+  messagingSenderId: "391838882934"
+};
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'FireBaseApp';
+
+export class AppComponent implements OnInit{
+  title = 'angular-7-firestore';
+  ngOnInit() {
+    firebase.initializeApp(config);
+    firebase.firestore().settings(settings);
+  }
 }
